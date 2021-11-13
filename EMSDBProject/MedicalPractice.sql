f80be1c5f8c60ac2900338490e2c3645448cc726
SLSmith <slsmit47@asu.edu>
2021-11-12 21:35:48 -0700

    test

---
+++ EMSDBProject/MedicalPractice.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MedicalPractice]
+(
+	[Practice_ID] INT NOT NULL PRIMARY KEY, 
+    [PracticeName] VARCHAR(50) NULL, 
+    [StreetAddress] NVARCHAR(50) NULL, 
+    [City] VARCHAR(50) NULL, 
+    [Zip] INT NULL, 
+    [Phone] INT NULL
+)
